79c6665c4f26f68c18713148a569901ad6bf8686
JT Cronin <jt.cronin@opscompass.com>
2021-08-21 15:52:13 -0500

    last years app

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/status') 
+def hello(): 
+    return "up"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
+@app.route('/rules')
+def rules():
+    return render_template('rules.html')
+
+@app.route('/cover')
+def cover():
+    return render_template('cover.html')
+
+@app.route('/test')
+def base():
+    return render_template('testBase.html')
+
+
+if __name__ == "__main__": 
+    app.run(host ='0.0.0.0', port = 8080, debug = True)
